aa04036215471bb223088614747157f4bcf3e5e5
WebictbyLeo <webictbyleo@gmail.com>
2025-07-01 15:14:40 +0100

    feat: Remove MakerBundle from development dependencies in bundles configuration

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -7,7 +7,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
